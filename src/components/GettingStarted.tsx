222edb59e6614988f071135bb7cc527468b56549
SuperDev <ilonka.melochek02@gmail.com>
2025-06-13 13:59:23 -0400

    social media marketing download added

--- src/components/GettingStarted.tsx
+++ src/components/GettingStarted.tsx
@@ -1,6 +1,52 @@
 const GettingStarted = () => (
   <div className="bg-white py-16 px-4 text-center">
     <h2 className="text-2xl font-bold mb-6">New to Social Media Marketing?</h2>
+    <div className="max-w-4xl mx-auto mb-8 border border-gray-300 p-4 border-rounded-xl bg-gray-100 ">
+      <p className="text-lg mb-4 text-center font-bold">Learn before you pay plan!</p>
+      <p className="mb-2 text-left">
+        Download these two epub books that contain the lessons you'll need to
+        use the Best Global AI program tools.
+      </p>
+      <p className="mb-2 text-left">
+       
+        <a
+          href="/ebooks/Ten_Lessons_Chief_Executive_Officer.epub"
+          download
+          className="text-blue-600  "
+        >
+           Download the Chief Executive Officer Lessons here.
+        </a>
+       
+      </p>
+      <p className="text-left mb-2">
+       
+        <a
+          href="/ebooks/Enhancing_Creative_Writing.epub"
+          download
+          className="text-blue-600 "
+        >
+           Download the Enhancing Creative Writing Lessons here.
+        </a>
+       
+      </p>
+      <p className="text-left mb-2">
+        {" "}
+        To read these ePub3 books use the Thorium eReader for PC and Mac here*{" "}
+        <a
+          href="https://thorium.edrlab.org/en/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          (link: https://thorium.edrlab.org/en/)
+        </a>
+      </p>
+
+      <p className="mb-4 text-left">
+        *Note: You won't be able to translate the text using the Thorium
+        eReader. You can only translate when you purchase the Single Product
+        Plan.
+      </p>
+    </div>
     <div className="flex flex-col md:flex-row justify-center gap-6">
       <div className="bg-gray-100 p-6 rounded shadow w-full max-w-sm">
         <h3 className="font-semibold mb-2">Start your journey at BGAI.</h3>
